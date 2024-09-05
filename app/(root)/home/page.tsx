8fea088cd6092039d8ae0fd1c6cba6aecded9e45
sreenadcr <124657269+sreenadcr@users.noreply.github.com>
2024-09-06 00:06:36 +0530

    hero

---
+++ app/(root)/home/page.tsx
@@ -0,0 +1,13 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+    <div>
+      <div className="bg-black text-white">
+       <div className="grid grid-cols-10">
+
+       </div>
+      </div>
+    </div>
+  );
+}
